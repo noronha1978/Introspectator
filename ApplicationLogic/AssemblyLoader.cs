97d74fb44827de0485deb6aace0de0949ca99bbc
noronha1978 <noronha1978@yahoo.com>
2009-10-13 00:00:22 -0400

    Running a spike to see the creation of abstracted assembly information

--- ApplicationLogic/AssemblyLoader.cs
+++ ApplicationLogic/AssemblyLoader.cs
@@ -23,12 +23,92 @@ namespace Introspectator.ApplicationLogic
             Type[] netTypes = assembly.GetTypes();
             foreach (Type netType in netTypes)
             {
-                ITypeInfo type = new TypeInfo();
-                //netType.Name;
-                //netType.Namespace;
-                //netType.
+                ITypeInfo typeInfo = CreateNewTypeInfo(netType);
+
+                typeInfo.Methods = GetMethodsFor(netType);
+                typeInfo.Properties = GetPropertiesFor(netType);
+                typeInfo.Events = GetEventsFor(netType);
+                typeInfo.Fields = GetFieldsFor(netType);
             }
             return types;
+        }
+
+        private IList<IFieldInfo> GetFieldsFor(Type netType)
+        {
+            IList<IFieldInfo> fields = new List<IFieldInfo>();
+            System.Reflection.FieldInfo[] netFields = netType.GetFields(BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (System.Reflection.FieldInfo netField in netFields)
+            {
+                IFieldInfo fieldInfo = new FieldInfo();
+                fieldInfo.Name = netField.Name;
+                fields.Add(fieldInfo);
+            }
+            return fields;
+        }
+
+        private IList<IEventInfo> GetEventsFor(Type netType)
+        {
+            IList<IEventInfo> events = new List<IEventInfo>();
+            System.Reflection.EventInfo[] netEvents = netType.GetEvents(BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (System.Reflection.EventInfo netEvent in netEvents)
+            {
+                IEventInfo eventInfo = new EventInfo();
+                eventInfo.Name = netEvent.Name;
+                events.Add(eventInfo);
+            }
+            return events;
+        }
+
+        private IList<IPropertyInfo> GetPropertiesFor(Type netType)
+        {
+            IList<IPropertyInfo> properties = new List<IPropertyInfo>();
+            System.Reflection.PropertyInfo[] netProperties = netType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (System.Reflection.PropertyInfo netProperty in netProperties)
+            {
+                IPropertyInfo propertyInfo = new PropertyInfo();
+                propertyInfo.Name = netProperty.Name;
+                properties.Add(propertyInfo);
+            }
+            return properties;
+        }
+
+        private IList<IMethodInfo> GetMethodsFor(Type netType)
+        {
+            IList<IMethodInfo> methods = new List<IMethodInfo>();
+            System.Reflection.MethodInfo[] netMethods = netType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (System.Reflection.MethodInfo netMethod in netMethods)
+            {
+                IMethodInfo methodInfo = new MethodInfo();
+                methodInfo.Name = netMethod.Name;
+                methods.Add(methodInfo);
+            }
+            return methods;
+        }
+
+
+        private ITypeInfo CreateNewTypeInfo(Type netType)
+        {
+            ITypeInfo type = null;
+            if (netType.IsAbstract)
+            {
+                type = new AbstractClass();
+            }
+            else if (netType.IsClass)
+            {
+                type = new Class();
+            }
+            else if (netType.IsInterface)
+            {
+                type = new Interface();
+            }
+            else if (netType.IsEnum)
+            {
+                type = new Enumeration();
+            }
+            type.Name = netType.Name;
+            type.Namespace = netType.Namespace;
+
+            return type;
         }
     }
 }
--- ApplicationLogic/IAssembly.cs
+++ ApplicationLogic/IAssembly.cs
@@ -45,31 +45,220 @@ namespace Introspectator.ApplicationLogic
 
     public interface ITypeInfo
     {
+        System.Collections.Generic.IList<IFieldInfo> Fields { get; set; }
+        System.Collections.Generic.IList<IEventInfo> Events { get; set; }
+        System.Collections.Generic.IList<IPropertyInfo> Properties { get; set; }
+        System.Collections.Generic.IList<IMethodInfo> Methods { get; set; }
+        string Namespace { get; set; }
+        string Name { get; set; }
 
     }
 
     public class TypeInfo:ITypeInfo
+    {
+        private IList<IFieldInfo> _fields;
+        private IList<IEventInfo> _events;
+        private IList<IPropertyInfo> _properties;
+        private IList<IMethodInfo> _methods;
+        private string _namespace;
+        private string _name;
+        
+        #region ITypeInfo Members
+
+        public IList<IFieldInfo> Fields
+        {
+            get
+            {
+                return _fields;
+            }
+            set
+            {
+                _fields = value;
+            }
+        }
+
+        public IList<IEventInfo> Events
+        {
+            get
+            {
+                return _events;
+            }
+            set
+            {
+                _events = value;
+            }
+        }
+
+        public IList<IPropertyInfo> Properties
+        {
+            get
+            {
+                return _properties;
+            }
+            set
+            {
+                _properties = value;
+            }
+        }
+
+        public IList<IMethodInfo> Methods
+        {
+            get
+            {
+                return _methods;
+            }
+            set
+            {
+                _methods = value;
+            }
+        }
+
+        public string Namespace
+        {
+            get
+            {
+                return _namespace;
+            }
+            set
+            {
+                _namespace = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        #endregion
+    }
+
+    public class AbstractClass : TypeInfo
+    {
+
+    }
+
+    public class Class : TypeInfo
+    {
+
+    }
+
+    public class Interface : TypeInfo
+    {
+
+    }
+
+    public class Enumeration : TypeInfo
     {
 
     }
 
     public interface IMethodInfo
     {
+        string Name { get; set; }
 
     }
 
-    public class MethodInfo
+    public class MethodInfo:IMethodInfo
     {
+        private string _name;
+        #region IMethodInfo Members
 
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        #endregion
     }
 
     public interface IPropertyInfo
     {
+        string Name { get; set; }
 
     }
 
-    public class PropertyInfo
+    public class PropertyInfo:IPropertyInfo
     {
+        private string _name;
+        #region IPropertyInfo Members
 
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        #endregion
+    }
+
+    public interface IEventInfo
+    {
+        string Name { get; set; }
+    }
+
+    public class EventInfo:IEventInfo
+    {
+        private string _name;
+        #region IEventInfo Members
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        #endregion
+    }
+
+    public interface IFieldInfo
+    {
+        string Name { get; set; }
+    }
+
+    public class FieldInfo : IFieldInfo
+    {
+        private string _name;
+        #region IEventInfo Members
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        #endregion
     }
 }
